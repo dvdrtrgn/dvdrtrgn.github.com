0067ef514716635cc270b84ae7773113aa61fa70
dvdrtrgn <dvdrtrgn@gmail.com>
2014-07-28 23:24:03 -0500

    get fancy

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function writeArea() {
     var te = document.getElementsByTagName('textarea')[0];
-    te.innerText = new Date();
+    te.innerText = '.\n' + new Date() + '\n.';
 }
 
 function enCode() {
-    var Form1 = document.forms[0];
-    for (var i = 0, thing; thing = Form1[i]; i++) {
-        console.log(thing);
-    }
+    console.log(document.forms[0]);
 }
+
+writeArea();
